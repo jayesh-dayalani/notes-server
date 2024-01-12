d6657d230c0cd9eccba3456c330879a7dee98f48
jayesh-dayalanii <jayesh.dayalani@picostone.com>
2024-01-13 03:01:45 +0530

    login

--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,15 @@
 const router = require('express').Router()
 const User = require('../models/Users')
 
-router.get('/login', (req, res) => {
-    res.send("user logged in")
-})
-
+// trial purpose
+// router.get('/login', (req, res) => {
+//     res.send("user logged in")
+// })
 // router.get('/register', (req,res) => {
 //     res.send("user registered ")
 // })
 
-// upload in mongo
+// upload in mongo : register
 router.post('/register', async (req, res) => {
 
     console.log(req.body)
@@ -30,4 +30,28 @@ router.post('/register', async (req, res) => {
         res.status(500).json(e)
     }
 })
+
+
+// check in mongo : login
+router.post('/login', async (req, res) => {
+
+    console.log(req.body)
+    try {
+        // checking email in db
+        const user = await User.findOne({ email: req.body.email })
+        !user && res.status(400).json({message:"user not found", status:false})
+
+        // validating password
+        const validatePassword = req.body.password == user.password
+        !validatePassword && res.status(400).json({message:"invalid password", status:false})
+
+        // on login successful
+        res.status(200).json(user)
+    }
+    catch (e) {
+        console.log("::err"+e)
+        res.status(500).json(e)
+    }
+})
+
 module.exports = router
